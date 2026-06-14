b160aeefbd7459635e441b3a235f4e31f101f267
Erlc <erlcapplication114@gmail.com>
2026-06-14 19:24:35 +0100

    Global structure & average calculation FINISHED

--- scriptcalcul.js
+++ scriptcalcul.js
@@ -1,29 +1,48 @@
 function calculer() {
     let matieres = [
-        {nom: "BDD", ids : ["td_bdd", "tp_bdd", "examen_bdd"], coefs: [0.2, 0.2, 0.6]},
-        {nom: "SE", ids : ["td_se", "tp_se", "examen_se"], coefs: [0.2, 0.2, 0.6]},
-        {nom: "DAW", ids : ["tp_daw", "examen_daw"], coefs: [0.4, 0.6]},
-        {nom: "POO", ids : ["tp_poo", "examen_poo"], coefs: [0.4, 0.6]},
-        {nom: "THL", ids: ["tp_thl", "td_thl", "examen_thl"], coefs: [0.2, 0.2, 0.6]},
-        {nom: "Reseau", ids: ["tp_reseau", "td_reseau", "examen_reseau"], coefs: [0.2, 0.2, 0.6]},
-        {nom: "Anglais", ids: ["examen_anglais"], coefs: [1]}
+        {nom: "BDD", ids : ["td_bdd", "tp_bdd", "examen_bdd"], coefs: [0.2, 0.2, 0.6], multiple :[3]},
+        {nom: "SE", ids : ["td_se", "tp_se", "examen_se"], coefs: [0.2, 0.2, 0.6], multiple :[3]},
+        {nom: "DAW", ids : ["tp_daw", "examen_daw"], coefs: [0.4, 0.6], multiple :[2]},
+        {nom: "POO", ids : ["tp_poo", "examen_poo"], coefs: [0.4, 0.6], multiple :[2]},
+        {nom: "THL", ids: ["tp_thl", "td_thl", "examen_thl"], coefs: [0.2, 0.2, 0.6], multiple :[2]},
+        {nom: "Reseau", ids: ["tp_reseau", "td_reseau", "examen_reseau"], coefs: [0.2, 0.2, 0.6] , multiple :[3]},
+        {nom: "Anglais", ids: ["examen_anglais"], coefs: [1] , multiple :[1]}
     ];
 
+    let somme = 0;
     matieres.forEach(matiere => {
         let moyenne = 0;
         matiere.ids.forEach((id, i) => {
             let note = document.getElementById(id).value || 0;
+            if (note < 0 || note > 20) {
+                alert("Veuillez entrer une note valide entre 0 et 20 pour " + matiere.nom);
+                return;
+            }
             moyenne += note * matiere.coefs[i];
+            
         });
         
+        somme += moyenne * matiere.multiple[0];
         let span = document.getElementById("moy_" + matiere.nom.toLowerCase());
 
         if (moyenne > 0) {
-            span.innerText = "Moyenne : " + moyenne;
+            span.innerText = "Moyenne : " + moyenne.toFixed(2);
             span.style.color = moyenne >= 10 ? "green" : "red";
         } else {
             span.innerText = "";
         }
-        console.log (matiere.nom + " : " + moyenne);
     });
+    let moy_generale = somme / 16;
+    let span_moyenne_generale = document.getElementById("moyenne_generale");
+    if (moy_generale > 0) {
+        span_moyenne_generale.innerText = "Votre moyenne générale est d'environ " + moy_generale.toFixed(2);
+        span_moyenne_generale.style.color = moy_generale >= 10 ? "green" : "red";
+    } else {
+        span_moyenne_generale.innerText = "Veuillez bien remplir les notes avant de lancer le calcul (si vous avez 0 veuillez mettre 0.01)";
+        span_moyenne_generale.style.color = "blue";
+    }
 }
+
+
+    document.querySelectorAll("input").forEach(input => {
+    input.addEventListener("input", calculer)});
